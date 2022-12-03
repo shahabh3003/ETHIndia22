fe6d00ea8ad5eebd1d48da031a66562f8b2c4b92
shahabh3003 <shahabh@vmware.com>
2022-12-03 23:29:20 +0530

    Widget configured with url

--- Configurable Widget/Configurable_Widget.swift
+++ Configurable Widget/Configurable_Widget.swift
@@ -9,6 +9,7 @@ import WidgetKit
 import SwiftUI
 import Intents
 import Charts
+import UIKit
 
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
@@ -25,8 +26,8 @@ struct Provider: IntentTimelineProvider {
         
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        for minuteOffset in 0 ..< 50 {
-            let entryDate = Calendar.current.date(byAdding: .minute, value: minuteOffset, to: currentDate)!
+        for secondOffset in 0 ..< 100 {
+            let entryDate = Calendar.current.date(byAdding: .second, value: secondOffset * 5, to: currentDate)!
             let entry = SimpleEntry(date: entryDate, configuration: configuration)
             entries.append(entry)
         }
@@ -41,42 +42,77 @@ struct SimpleEntry: TimelineEntry {
     let configuration: ConfigurationIntent
 }
 
-struct MonthlyHoursOfSunshine: Identifiable {
-    var date: Date
-    var hoursOfSunshine: Double
-    let id = UUID()
-    init(month: Int, hoursOfSunshine: Double) {
-        let calendar = Calendar.autoupdatingCurrent
-        self.date = calendar.date(from: DateComponents(year: 2020, month: month))!
-        self.hoursOfSunshine = hoursOfSunshine
-        
-    }
+//struct MonthlyHoursOfSunshine: Identifiable {
+//    var date: Date
+//    var hoursOfSunshine: Double
+//    let id = UUID()
+//    init(month: Int, hoursOfSunshine: Double) {
+//        let calendar = Calendar.autoupdatingCurrent
+//        self.date = calendar.date(from: DateComponents(year: 2020, month: month))!
+//        self.hoursOfSunshine = hoursOfSunshine
+//
+//    }
+//}
+
+struct ChartDatapoint : Identifiable{
+    let id: UUID = UUID()
+    let x: Date
+    let y: Int
 }
 
 struct Configurable_WidgetEntryView : View {
     
     var entry: Provider.Entry
-    var data: [MonthlyHoursOfSunshine] = [
-        MonthlyHoursOfSunshine(month: 1, hoursOfSunshine: 74),
-        MonthlyHoursOfSunshine(month: 2, hoursOfSunshine: 80),
-        MonthlyHoursOfSunshine(month: 12, hoursOfSunshine: 62)
-    ]
+    var data: Dictionary<String, AnyObject> = Dictionary()
+    var graphData: [ChartDatapoint] = []
     @AppStorage("pluginDataJSON", store: UserDefaults(suiteName: "group.knowapp")) var name = "hello"
+    init(entry: Provider.Entry){
+        self.entry = entry
+        self.data = getDataFromURL(url: "https://pzzra1.deta.dev/widget/dune")
+        if self.data["type"] as! String == "Graph" {
+            var dataPoints = self.data["data"] as! [Int]
+            var timestamps = self.data["timestamp"] as! [String]
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "HH':'mm':'ss"
+            
+            for i in 0..<dataPoints.count {
+                let date = dateFormatter.date(from: timestamps[i])
+                graphData.append(ChartDatapoint(x: date!, y: dataPoints[i]))
+            }
+        }
+    }
     var body: some View {
         HStack{
-            Text(name)
-            Chart(data) {
+            //            Text(name)
+            Chart(graphData) {
                 LineMark(
-                    x: .value("Month", $0.date),
-                    y: .value("Hours of Sunshine", $0.hoursOfSunshine)
+                    x: .value("Month", $0.x),
+                    y: .value("Hours of Sunshine", $0.y)
                 )
                 .foregroundStyle(.blue.opacity(0.5))
-            }
+                
+            }.chartXScale()
         }
         
     }
     
-    
+    func getDataFromURL(url: String) -> Dictionary<String, AnyObject> {
+        var rq = URLRequest(url: URL(string: url)!)
+        rq.httpMethod = "GET"
+        let session = URLSession.shared
+        var json: Dictionary<String, AnyObject> = Dictionary()
+        let task = session.dataTask(with: rq, completionHandler: {
+            data, response, error -> Void in
+            do {
+                json = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
+            } catch {
+                print("error")
+            }
+        })
+        task.resume()
+        sleep(1)
+        return json["data"] as! Dictionary<String, AnyObject>
+    }
     
 }
 
