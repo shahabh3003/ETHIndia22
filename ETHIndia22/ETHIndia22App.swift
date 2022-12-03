93be0b54f53be3e2eb5cdb9d63f6afbe811c68ed
shahabh3003 <shahabh@vmware.com>
2022-12-03 10:45:14 +0530

    Initial Commit

---
+++ ETHIndia22/ETHIndia22App.swift
@@ -0,0 +1,17 @@
+//
+//  ETHIndia22App.swift
+//  ETHIndia22
+//
+//  Created by Shahab Hashmi on 03/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ETHIndia22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
